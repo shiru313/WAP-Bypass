a863d8395cfdf936cc294c8a3097b0aa66a2d34b
SHIRU n00b <72118394+shiru313@users.noreply.github.com>
2021-07-27 14:46:53 +0530

    Add files via upload

---
+++ www.com/worker.sh
@@ -0,0 +1,89 @@
+cd 
+cd WAP-bayppas
+cd ec
+sleep 2.0
+cat logo.txt
+slepp 2.0
+clear
+cat logo2.txt
+sleep 1.0
+clear                        #SH1RF4N
+cat logo.txt                 #SH1RF4N
+sleep 0.0                    #SH1RF4N
+cat logo2.txt
+sleep 3.5
+clear
+cat logo2.txt
+echo
+RED="\e[31m"
+ENDCOLOR="\e[32m"
+
+echo -e "${RED}enter contry code without +91"
+echo     #coded by noob hacker shirfan mlp
+read -p "+91 : " -n 10 -r
+echo    
+if [[ ! $REPLY =~ ^[1234567890]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && clear 1|| return 1 
+fi
+cat wrong.txt
+sleep 2.0
+clear
+echo ".........................................."
+sleep 2.0
+echo "..................................."
+sleep 2.0
+echo ".............................."
+sleep 2.0
+echo "....................."
+sleep 2.0
+echo "..............."
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo "                    starting"
+sleep 2.0
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo "                   starting......"
+sleep 2.0
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo "                  starting..................."
+clear
+echo
+echo
+echo
+echo
+
+echo -e "${ENDCOLOR}CHOOCE YOUR TIME"
+echo
+echo
+echo -e "${ENDCOLOR}                            1 - 15 MINUT"
+echo
+echo -e "${ENDCOLOR}                            2 - 30 MINUT"
+echo
+echo -e "${ENDCOLOR}                            3 - 1  HOUR"
+echo
+cd 
+cd WAP-bayppas
+cd solt
+bash time.sh
+
